d085a81202d08f642a9e15fb826049f1cab9e41c
JSRossiter <jeffrey.rossiter@me.com>
2017-04-18 21:24:06 +0000

    posting/loading/rendering tweets + compose button

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -4,7 +4,7 @@
  * Reminder: Use (and do all your DOM work in) jQuery's document ready function
  */
 
- function createTweetElement (input) {
+function createTweetElement (input) {
   const $tweet = $("<article>");
   const $header = $("<header>");
   const $avatar = $("<img>").addClass("avatar").attr("src", input.user.avatars.small);
@@ -15,7 +15,7 @@
   const $div = $("<div>").addClass("tweet-content").text(input.content.text);
 
   const ageMinutes = Math.floor((Date.now() - input.created_at) / (1000 * 60));
-  let ageStatement = ""
+  let ageStatement = "";
   if (ageMinutes === 0) {
     ageStatement = "Just now";
   } else if (ageMinutes < 60) {
@@ -37,63 +37,65 @@
 
   $tweet.append($header, $div, $footer);
   return $tweet;
- }
+}
 
- function renderTweets (tweets) {
+function renderTweets (tweets) {
   for (var i = 0; i < tweets.length; i++) {
-    $("#tweets-container").append(createTweetElement(tweets[i]));
+    $("#tweets-container").prepend(createTweetElement(tweets[i]));
   }
- }
+}
 
-var data = [
-  {
-    "user": {
-      "name": "Newton",
-      "avatars": {
-        "small":   "https://vanillicon.com/788e533873e80d2002fa14e1412b4188_50.png",
-        "regular": "https://vanillicon.com/788e533873e80d2002fa14e1412b4188.png",
-        "large":   "https://vanillicon.com/788e533873e80d2002fa14e1412b4188_200.png"
-      },
-      "handle": "@SirIsaac"
-    },
-    "content": {
-      "text": "If I have seen further it is by standing on the shoulders of giants"
-    },
-    "created_at": Date.now() - 1000000
-  },
-  {
-    "user": {
-      "name": "Descartes",
-      "avatars": {
-        "small":   "https://vanillicon.com/7b89b0d8280b93e2ba68841436c0bebc_50.png",
-        "regular": "https://vanillicon.com/7b89b0d8280b93e2ba68841436c0bebc.png",
-        "large":   "https://vanillicon.com/7b89b0d8280b93e2ba68841436c0bebc_200.png"
-      },
-      "handle": "@rd" },
-    "content": {
-      "text": "Je pense , donc je suis"
-    },
-    "created_at": Date.now()
-  },
-  {
-    "user": {
-      "name": "Johann von Goethe",
-      "avatars": {
-        "small":   "https://vanillicon.com/d55cf8e18b47d4baaf60c006a0de39e1_50.png",
-        "regular": "https://vanillicon.com/d55cf8e18b47d4baaf60c006a0de39e1.png",
-        "large":   "https://vanillicon.com/d55cf8e18b47d4baaf60c006a0de39e1_200.png"
-      },
-      "handle": "@johann49"
-    },
-    "content": {
-      "text": "Es ist nichts schrecklicher als eine tätige Unwissenheit."
-    },
-    "created_at": 1461113796368
+function flashMessage (message) {
+  const span = $("<span>").addClass("flash-message").text(message);
+  $("form").append(span);
+  span.slideDown(function() {
+    setTimeout(function() {
+      span.slideUp();
+    }, 2000);
+  });
+}
+
+function loadTweets () {
+  $.ajax({
+    url: "/tweets/",
+    method: 'GET',
+    success: renderTweets
+  });
+}
+
+function postTweet (event) {
+  event.preventDefault();
+  const tweet = $("textarea[name='text']");
+  if (!tweet[0].value) {
+    flashMessage("You didn't type anything!");
+  } else if (tweet[0].value.length > 140) {
+    flashMessage("Your tweet is too long!");
+  } else {
+    $.ajax({
+      url: "/tweets/",
+      method: "POST",
+      data: tweet.serialize(),
+      success: function () {
+        loadTweets();
+        tweet[0].value = "";
+      }
+    });
   }
-];
+}
+
+function composeButton (event) {
+  event.preventDefault();
+  $(".new-tweet").slideToggle(function() {
+    if (!$(".new-tweet").is(":hidden")) {
+      $(".new-tweet textarea").focus();
+    }
+  });
+
+}
 
 
 $(document).ready(function() {
-  renderTweets(data);
-
-})
+  loadTweets();
+  $(".compose").click(composeButton);
+  $("input[value='Tweet']").click(postTweet);
+});
--- public/scripts/composer-char-counter.js
+++ public/scripts/composer-char-counter.js
@@ -1,12 +1,11 @@
 $(document).ready(function() {
   let charCount = 140;
-  $(".new-tweet textarea").on("keydown", function(event) {
+  $(".new-tweet textarea").on("input", function(event) {
     charCount = 140 - $(this).val().length;
     const counter = $(this).siblings(".counter");
     if (charCount < 0) {
-      counter.addClass("invalid");;
+      counter.addClass("invalid");
     }
     counter.text(charCount);
-    console.log(counter);
-  })
-})
+  });
+});
